d65727a4c50d7c007ecc5a5147f8844afa46cf00
spiffytech <git@spiffy.tech>
2021-10-28 15:19:30 -0400

    Set a secure cookie with a samesite value

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ fastify.get("/", async (request, reply) => {
   const cookieValue = request.cookies.mySecureCookie;
   reply.type("text/html");
   if (!cookieValue) {
-    reply.setCookie("mySecureCookie", new Date().toString());
+    reply.setCookie("mySecureCookie", new Date().toString(), {
+      secure: true,
+      sameSite: "lax",
+    });
     reply.send(`<p data-cy="no-cookie">Request didn't have a cookie</p>`);
   } else {
     reply.send(`<p data-cy="cookie">Cookie value: ${cookieValue}</p>`);
